86ae3d39436a8a7085b3f34595f488b2b9202364
alanbispo <alannmg1@gmail.com>
2026-02-25 09:37:03 -0300

    feat: utilizando variavel de ambiente VITE_API_URL

--- frontend/src/api/chatService.ts
+++ frontend/src/api/chatService.ts
@@ -10,7 +10,7 @@ export interface OffersResponse {
 }
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 export const sendMessageToBot = async (message: string): Promise<string> => {
